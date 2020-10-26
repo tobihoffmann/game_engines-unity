7ef996d0a6d03187d31438713c499c3dfa2fd7b2
Tobi Hoffmann <tobi.hoffmann@protonmail.com>
2020-10-26 14:54:37 +0100

    initial

---
+++ Assets/Scripts/AbstractClasses/Damageable.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+
+namespace AbstractClasses
+{
+    public abstract class Damageable : MonoBehaviour
+    {
+        /// <summary>
+        /// The actual hit points of the entity
+        /// </summary>
+        [SerializeField] [Tooltip("current hitpoints of the entity")]
+        public int hitPoints;
+        
+        /// <summary>
+        /// Used to prevent attacking a dead entity.
+        /// </summary>
+        public bool IsDead { get; private set; }
+        
+        /// <summary>
+        /// Does damage to the entity
+        /// </summary>
+        public abstract void Hit(int damage);
+
+        /// <summary>
+        /// Heals to the entity
+        /// </summary>
+        public abstract void Heal(int hitPoints);
+
+
+        /// <summary>
+        /// Lets the entity die and prevents multiple deaths.
+        /// </summary>
+        protected virtual void Die()
+        {
+            if (IsDead)
+                return;
+            IsDead = true;
+            
+            Debug.Log("Entity is dead.");
+        }
+    }
+}
---
+++ Assets/Scripts/Entity/Player/PlayerState.cs
@@ -0,0 +1,54 @@
+﻿using AbstractClasses;
+using UnityEngine;
+
+namespace Entity.Player
+{
+    public class PlayerState : Damageable
+    {
+        public delegate void PlayerStateChanged(int newValue);
+        
+        public delegate void PlayerIsDead();
+
+        /// <summary>
+        /// The maximum hit points of the player
+        /// </summary>
+        [SerializeField] [Tooltip("maximum hit points of the player")]
+        private int maxHitPoints;
+        
+        public static event PlayerStateChanged OnPlayerHitPointsUpdate;
+        
+        public static event PlayerIsDead OnPlayerDeath;
+        
+        
+        /// <summary>
+        /// changes the entities current hit points
+        /// </summary>
+        public void ChangePlayerHitPoints(int changeBy)
+        {
+            int updatedValue = hitPoints + changeBy;
+            if (updatedValue > maxHitPoints) updatedValue = maxHitPoints;
+            updatedValue = Mathf.Clamp(updatedValue, 0, maxHitPoints);
+            hitPoints = updatedValue;
+            //throws an event with the new health value as a parameter
+            OnPlayerHitPointsUpdate?.Invoke(hitPoints);
+            if (hitPoints <= 0) 
+                Die();
+        }
+
+        public override void Hit(int damage)
+        {
+            ChangePlayerHitPoints(-damage);
+        }
+
+        public override void Heal(int healAmount)
+        {
+           ChangePlayerHitPoints(healAmount);
+        }
+        
+        protected override void Die()
+        {
+            base.Die();
+            OnPlayerDeath?.Invoke();
+        }
+    }
+}
