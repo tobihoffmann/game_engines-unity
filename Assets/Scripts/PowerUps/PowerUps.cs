a6de819ad0a60bf01f5f2e086c7888b867e9dfd0
tobihoffmann <38102896+tobihoffmann@users.noreply.github.com>
2020-11-03 09:30:32 +0100

    Level stuff (#21)
    * init
    
    * added prefabs, art
    
    * fixed missing collider on prefab

---
+++ Assets/Scripts/PowerUps/PowerUps.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+
+namespace PowerUps
+{
+    [CreateAssetMenu(fileName = "New Power-Up", menuName = "Power-Up")]
+    public class PowerUp : ScriptableObject
+    {
+        [SerializeField]
+        private string title;
+        
+        [SerializeField]
+        private Sprite icon;
+        
+        [SerializeField]
+        private int bonusHp;
+        
+        [SerializeField]
+        private int bonusAmmo;
+        
+        [SerializeField]
+        private int bonusMeleeDamage;
+        
+        [SerializeField]
+        private int bonusRangeDamage;
+        
+        [SerializeField]
+        private WeaponModifier weaponMod;
+    }
+}
