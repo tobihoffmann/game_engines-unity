640efa7291cca8ba908664983c5ac29d3bfa755d
KBuklewski <45571879+KBuklewski@users.noreply.github.com>
2020-11-04 18:06:53 +0100

    Shooting in all directions, explosion animation
    shooting in all directions is now possible, but we need new sprites for the character to look in different directions or fix the rotation of the character sprite
    added animation for explosion

---
+++ Assets/BulletCollider.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Security.Cryptography;
+using UnityEngine;
+
+public class BulletCollider : MonoBehaviour
+{
+
+    public GameObject hitEffect;
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        GameObject effect = Instantiate(hitEffect, transform.position, Quaternion.identity);
+        Destroy(effect, 0.15f);
+        Destroy(gameObject);
+
+    }
+}
--- Assets/shooting.cs
+++ Assets/shooting.cs
@@ -4,22 +4,17 @@ using UnityEngine.InputSystem;
 
 public class shooting : MonoBehaviour
 {
-    public Camera cam;
-    public Rigidbody2D prb;
+    
     public Transform firePoint;
     public GameObject bulletPrefab;
     public float bulletForce = 20f;
-    //public InputMaster controls;
     [SerializeField][Tooltip("Input actions for shooting")]
     private InputAction shoot;
 
 
-    // Update is called once per frame
+    
 
-    private Vector2 _mousePosition;
-    private Vector2 _playerPosition;
-    private Vector2 _dashDirection;
-    private Vector2 mousePos;
+    
 
     private void OnEnable()
     {
@@ -31,22 +26,7 @@ public class shooting : MonoBehaviour
     }
 
     void Update()
-    {
-        /*
-        _mousePosition = mainCam.ScreenToWorldPoint(Mouse.current.position.ReadValue());
-        _playerPosition = new Vector2(transform.position.x, transform.position.y);
-        _dashDirection = new Ray2D(_playerPosition, _mousePosition - _playerPosition).direction;
-        */
-        
-
-        mousePos = cam.ScreenToWorldPoint(Mouse.current.position.ReadValue());
-        Vector2 lookDir = (Vector3)mousePos - firePoint.position;
-
-        float angle = Mathf.Atan2(lookDir.y, lookDir.x) * Mathf.Rad2Deg - 90f;
-        firepoint.rotation = angle;
-        
-
-
+    { 
         if (shoot.triggered)
         {
             Shoot();
