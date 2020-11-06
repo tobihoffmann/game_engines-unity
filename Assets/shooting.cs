10601522f5b3a12b80b8a81385e5b4005023471b
KBuklewski <45571879+KBuklewski@users.noreply.github.com>
2020-11-06 11:43:25 +0100

    fixed some problems
    Player now shoots in all directions, layer ignore has to be fixed

--- Assets/shooting.cs
+++ Assets/shooting.cs
@@ -4,12 +4,12 @@ using UnityEngine.InputSystem;
 
 public class shooting : MonoBehaviour
 {
-     [SerializeField][Tooltip("Main Camera Object")]
-     private Camera mainCam;
+    [SerializeField][Tooltip("Main Camera Object")]
+    private Camera mainCam;
     
     public Transform firePoint;
     public GameObject bulletPrefab;
-    public float bulletForce = 20f;
+    public float bulletForce = 50f;
     [SerializeField][Tooltip("Input actions for shooting")]
     private InputAction shoot;
 
@@ -42,19 +42,25 @@ public class shooting : MonoBehaviour
 
     void Shoot()
     {
-        
-        GameObject bullet = Instantiate(bulletPrefab, firePoint.position, firePoint.rotation);
-        
-        
-        Rigidbody2D rb = bullet.GetComponent<Rigidbody2D>();
 
         _mousePosition = mainCam.ScreenToWorldPoint(Mouse.current.position.ReadValue());
         _playerPosition = new Vector2(transform.position.x, transform.position.y);
-        _shootDirection = new Ray2D(_playerPosition,_mousePosition - _playerPosition).direction;
+        _shootDirection = new Ray2D(_playerPosition, _mousePosition - _playerPosition).direction;
+
+        
+        //initiate the bullet
+        GameObject bullet = Instantiate(bulletPrefab, firePoint.position, firePoint.rotation);
+        
+        Rigidbody2D rb = bullet.GetComponent<Rigidbody2D>();
+
+        //turn the bullet in the correct direction
+        float angle = Mathf.Atan2(_shootDirection.y, _shootDirection.x) * Mathf.Rad2Deg - 90f;
+        rb.rotation = angle;
+
+        //addForce in direction of Mouse position
         rb.AddForce(_shootDirection * bulletForce, ForceMode2D.Impulse);
 
-        float angle = Mathf.Atan2(_shootDirection.y, _shootDirection.x) * Mathf.Rad2Deg - 90;
-        rb.rotation = angle;
+        
     }
     
 }
