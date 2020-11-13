ae3f353bd50f6b6f059e4bb3ef094bbd99e9b69b
Tobi Hoffmann <tobi.hoffmann@protonmail.com>
2020-11-13 18:40:22 +0100

    initial random level generator scripts

---
+++ Assets/Scripts/LevelGeneration/LevelGenerator.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelGenerator : MonoBehaviour
+{
+    private void ManageJunction()
+    {
+        // this is property
+        int junctionsOfThree;
+        int junctionsOfFour;
+        // 1. check if new tile is junction
+            // if junctionsOfThree >= maxJunctionsOfThree || if junctionsOfFour >= maxJunctionsOfFour
+                // if junctionsOfThree >= maxJunctionsOfThree
+                    // then delete all junctions of 3 from arrays
+                // else if junctionsOfFour >= maxJunctionsOfFour
+                    // then delete all junctions of 4 from arrays
+            // else
+                // check which junction (3,4)?
+                    // if 3 then junctionsOfThree++
+                        // if junctionsOfThree >= maxJunctionsOfThree
+                        // then delete all junctions of 3 from arrays
+                    // else 4 then junctionsOfFour++
+    }
+
+    private bool canPlaceJunction()
+    {
+        // if junctionsOf4 >= maxJunctionsOf4 || junctionsOf3 >= maxJunctionsOf3 || is previous tile a junction
+            // return false
+        // else return true
+        return false;
+    }
+}
---
+++ Assets/Scripts/LevelGeneration/TileTemplates.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+namespace LevelGeneration
+{
+    public class TileTemplates : MonoBehaviour
+    {
+        public GameObject[] northTiles;
+
+        public GameObject[] eastTiles;
+
+        public GameObject[] southTiles;
+    
+        public GameObject[] westTiles;
+    }
+}
