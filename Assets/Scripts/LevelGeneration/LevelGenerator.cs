40d2fd4eba77d5995e02196df9c9f6f56e9f4b09
jakobhes <jheschl95@gmail.com>
2020-11-13 19:22:28 +0100

    inital algo, in progress

--- Assets/Scripts/LevelGeneration/LevelGenerator.cs
+++ Assets/Scripts/LevelGeneration/LevelGenerator.cs
@@ -1,27 +1,92 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿
+using LevelGeneration;
 using UnityEngine;
 
 public class LevelGenerator : MonoBehaviour
 {
+    private int _junctionsThree;
+    private int _junctionsFour;
+    
+    private int _maxJunctionsThree;
+    private int _maxJunctionsFour;
+    
+
+    private TileTemplates _templates;
+
+    private int _random;
+
+    private bool _isSpawned;
+
+    [SerializeField][Tooltip("Entry tile")]
+    private GameObject _entryPoint;
+        
+    /// <summary>
+    /// The road opening needed on the new tile to spawn:
+    /// North, East, South, West
+    /// </summary>
+    [SerializeField][Tooltip("Amount of Tiles the level is going to exist of")]
+    private int levelSize;
+
+    private void Start()
+    {
+        _templates = GameObject.FindGameObjectWithTag("RoadTiles").GetComponent<TileTemplates>();
+        Invoke(nameof(Spawn), 0.3f);
+    }
+    
+    private void Spawn()
+    {
+        // spawn point aus dem prefab _entryPoint als variable
+        for (int i = 0; i < levelSize; i++)
+        {
+            if (!_isSpawned)
+            {
+                switch (roadDirection)
+                {
+                    case Direction.North:
+                        _random = Random.Range(0, _templates.northTiles.Length);
+                        Instantiate(_templates.northTiles[_random], transform.position, Quaternion.identity);
+                        break;
+                    case Direction.East:
+                        _random = Random.Range(0, _templates.eastTiles.Length);
+                        Instantiate(_templates.eastTiles[_random], transform.position, Quaternion.identity);
+                        break;
+                    case Direction.South:
+                        _random = Random.Range(0, _templates.southTiles.Length);
+                        Instantiate(_templates.southTiles[_random], transform.position, Quaternion.identity);
+                        break;
+                    case Direction.West:
+                        _random = Random.Range(0, _templates.westTiles.Length);
+                        Instantiate(_templates.westTiles[_random], transform.position, Quaternion.identity);
+                        break;
+                    default:
+                        Debug.Log("There is no direction set on this spawn point");
+                        break;
+                }
+                _isSpawned = true;
+            }
+        }
+    }
+    
+    
     private void ManageJunction()
     {
-        // this is property
-        int junctionsOfThree;
-        int junctionsOfFour;
-        // 1. check if new tile is junction
-            // if junctionsOfThree >= maxJunctionsOfThree || if junctionsOfFour >= maxJunctionsOfFour
-                // if junctionsOfThree >= maxJunctionsOfThree
-                    // then delete all junctions of 3 from arrays
-                // else if junctionsOfFour >= maxJunctionsOfFour
-                    // then delete all junctions of 4 from arrays
-            // else
-                // check which junction (3,4)?
-                    // if 3 then junctionsOfThree++
-                        // if junctionsOfThree >= maxJunctionsOfThree
-                        // then delete all junctions of 3 from arrays
-                    // else 4 then junctionsOfFour++
+        if (newTile.isJunction)
+        {
+            if (newTile.isJunctionThree)
+            {
+                _junctionsThree++;
+                if (_junctionsThree >= _maxJunctionsThree)
+                    removealljunctionsthree();
+            } else if (newTile.isJunctionFour)
+            {
+                _junctionsFour++;
+                if (_junctionsFour >= _maxJunctionsFour)
+                    removealljunctionsfour();
+            }
+        }
     }
+    
+    
 
     private bool canPlaceJunction()
     {
--- Assets/Scripts/LevelGeneration/TileSpawner.cs
+++ Assets/Scripts/LevelGeneration/TileSpawner.cs
@@ -7,18 +7,15 @@ namespace LevelGeneration
 {
     public class TileSpawner : MonoBehaviour
     {
-        private enum Direction { North, East, South, West }
+        public enum Direction { North, East, South, West }
         
         /// <summary>
         /// The road opening needed on the new tile to spawn:
         /// North, East, South, West
         /// </summary>
         [SerializeField][Tooltip("road opening needed on the new tile to spawn")]
-        private Direction roadDirection;
-
-        private TileTemplates _templates;
-
-        private int _random;
+        public Direction roadDirection;
+        
 
         private bool _isSpawned;
         
@@ -28,42 +25,7 @@ namespace LevelGeneration
         /// </summary>
         [SerializeField][Tooltip("Amount of Tiles the level is going to exist of")]
         private int levelSize;
-
-        private void Start()
-        {
-            _templates = GameObject.FindGameObjectWithTag("RoadTiles").GetComponent<TileTemplates>();
-            Invoke(nameof(Spawn), 0.3f);
-        }
-
-        private void Spawn()
-        {
-            if (!_isSpawned)
-            {
-                switch (roadDirection)
-                {
-                    case Direction.North:
-                        _random = Random.Range(0, _templates.northTiles.Length);
-                        Instantiate(_templates.northTiles[_random], transform.position, Quaternion.identity);
-                        break;
-                    case Direction.East:
-                        _random = Random.Range(0, _templates.eastTiles.Length);
-                        Instantiate(_templates.eastTiles[_random], transform.position, Quaternion.identity);
-                        break;
-                    case Direction.South:
-                        _random = Random.Range(0, _templates.southTiles.Length);
-                        Instantiate(_templates.southTiles[_random], transform.position, Quaternion.identity);
-                        break;
-                    case Direction.West:
-                        _random = Random.Range(0, _templates.westTiles.Length);
-                        Instantiate(_templates.westTiles[_random], transform.position, Quaternion.identity);
-                        break;
-                    default:
-                        Debug.Log("There is no direction set on this spawn point");
-                        break;
-                }
-                _isSpawned = true;
-            }
-        }
+        
 
         private void OnTriggerEnter2D(Collider2D other)
         {
