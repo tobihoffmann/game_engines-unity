dba3edd985a11fdb4f3ecb782e54d5782cd693c4
Tobi Hoffmann <tobi.hoffmann@protonmail.com>
2020-11-16 19:40:48 +0100

    init

---
+++ Assets/Scripts/UI/HealthBar.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Linq;
+using UnityEngine;
+using UnityEngine.Serialization;
+using UnityEngine.UI;
+
+namespace UI
+{
+    public class HealthBar : MonoBehaviour
+    {
+        //public string SpriteLocation = "Sprites/PlayerHungerStates_Sprite";
+        
+        [SerializeField] 
+        private int currentHealth = 5;
+        
+        [SerializeField] 
+        private int maxHealth;
+
+        [SerializeField] 
+        private Image[] hearts;
+
+        [SerializeField] 
+        private Sprite fullHeart;
+        
+        [SerializeField] 
+        private Sprite emptyHeart;
+    
+    
+        // Start is called before the first frame update
+
+
+        private void Update()
+        {
+            if (currentHealth > maxHealth)
+            {
+                currentHealth = maxHealth;
+            }
+            
+            for (int i = 0; i < hearts.Length; i++)
+            {
+                if (i < currentHealth)
+                {
+                    hearts[i].sprite = fullHeart;
+                }
+                else
+                {
+                    hearts[i].sprite = emptyHeart;
+                }
+                if (i < maxHealth)
+                {
+                    hearts[i].enabled = true;
+                }
+                else
+                {
+                    hearts[i].enabled = false;
+                }                
+            }
+        }
+
+        private void Awake()
+        {
+            
+        }
+    
+        public void UpdateBar(int newValue)
+        {
+           
+
+        }
+
+        private void ChangeSprite()
+        {
+        }
+    
+    
+    }
+}
+                     
