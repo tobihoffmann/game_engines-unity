2d99fb5f5c590568079c7ae70198403ea51cd290
KBuklewski <45571879+KBuklewski@users.noreply.github.com>
2020-11-16 21:13:17 +0100

    Fighting mechanics noplayerrotation (#31)
    * First fighting mechanics.
    
    shooting direction is not working yet
    
    * trying around to figuring out how to rotate the firePoint
    
    * Shooting in all directions, explosion animation
    
    shooting in all directions is now possible, but we need new sprites for the character to look in different directions or fix the rotation of the character sprite
    added animation for explosion
    
    * init
    
    * rename
    
    * fixed some problems
    
    Player now shoots in all directions, layer ignore has to be fixed
    
    * Player trifft sich nicht mehr selbst
    
    war ganz easy, man kann in den Physics setting layer festlegen die sich nicht treffen sollen...
    
    * Changed required changes
    
    hab jetzt nochmal alles angepasst, umbenannt, verschoben und flott gemacht, hoffe das kann jetzt so gemerged werden :D
    
    * Set fields private
    
    Set fields private
    
    Co-authored-by: Tobi Hoffmann <tobi.hoffmann@protonmail.com>

--- Assets/Scripts/Entity/Player/PlayerMovementDash.cs
+++ Assets/Scripts/Entity/Player/PlayerMovementDash.cs
@@ -1,4 +1,5 @@
 ﻿
+using System.Collections.Specialized;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
---
+++ Assets/Scripts/Entity/shooting.cs
@@ -0,0 +1,69 @@
+﻿
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class shooting : MonoBehaviour
+{
+    [SerializeField][Tooltip("Main Camera Object")]
+    private Camera mainCam;
+
+    [SerializeField][Tooltip("FirePoint Object")]
+    private Transform firePoint;
+    [SerializeField][Tooltip("Prefab for Bullet Object")]
+    private GameObject bulletPrefab;
+    [SerializeField][Tooltip("Float variable for the Bulletforce")]
+    private float bulletForce = 50f;
+    [SerializeField][Tooltip("Input actions for shooting")]
+    private InputAction shoot;
+
+    private Vector2 _mousePosition;
+    private Vector2 _playerPosition;
+    private Vector2 _shootDirection;
+    
+
+    
+
+    private void OnEnable()
+    {
+        shoot.Enable();
+    }
+    private void OnDisable()
+    {
+        shoot.Disable();
+    }
+
+    void Update()
+    { 
+        if (shoot.triggered)
+        {
+            Shoot();
+        }
+        
+        
+    }
+
+
+    void Shoot()
+    {
+
+        _mousePosition = mainCam.ScreenToWorldPoint(Mouse.current.position.ReadValue());
+        _playerPosition = new Vector2(transform.position.x, transform.position.y);
+        _shootDirection = new Ray2D(_playerPosition, _mousePosition - _playerPosition).direction;
+
+        
+        //initiate the bullet
+        GameObject bullet = Instantiate(bulletPrefab, firePoint.position, firePoint.rotation);
+        
+        Rigidbody2D rb = bullet.GetComponent<Rigidbody2D>();
+
+        //turn the bullet in the correct direction
+        float angle = Mathf.Atan2(_shootDirection.y, _shootDirection.x) * Mathf.Rad2Deg - 90f;
+        rb.rotation = angle;
+
+        //addForce in direction of Mouse position
+        rb.AddForce(_shootDirection * bulletForce, ForceMode2D.Impulse);
+
+        
+    }
+    
+}
