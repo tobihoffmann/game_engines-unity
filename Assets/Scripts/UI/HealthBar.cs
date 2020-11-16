4dfca7d743cad15a2c5c9a642dbac501ea127314
Tobi Hoffmann <tobi.hoffmann@protonmail.com>
2020-11-16 21:09:33 +0100

    HealthBar now listens to PlayerState.cs

--- Assets/Scripts/UI/HealthBar.cs
+++ Assets/Scripts/UI/HealthBar.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Linq;
+using Entity.Player;
 using UnityEngine;
 using UnityEngine.Serialization;
 using UnityEngine.UI;
@@ -9,70 +10,50 @@ namespace UI
     public class HealthBar : MonoBehaviour
     {
         //public string SpriteLocation = "Sprites/PlayerHungerStates_Sprite";
-        
-        [SerializeField] 
-        private int currentHealth = 5;
-        
-        [SerializeField] 
-        private int maxHealth;
 
-        [SerializeField] 
-        private Image[] hearts;
+        [SerializeField] private int currentHealth = 5;
 
-        [SerializeField] 
-        private Sprite fullHeart;
-        
-        [SerializeField] 
-        private Sprite emptyHeart;
-    
-    
-        // Start is called before the first frame update
+        [SerializeField] private int maxHealth;
 
+        [SerializeField] private Image[] hearts;
 
-        private void Update()
+        [SerializeField] private Sprite fullHeart;
+
+        [SerializeField] private Sprite emptyHeart;
+
+        private void OnEnable()
         {
-            if (currentHealth > maxHealth)
-            {
-                currentHealth = maxHealth;
-            }
+            PlayerState.OnPlayerHitPointsUpdate += OnHealthUpdated;
+            //TODO: PlayerState.OnPlayerPowerUpsUpdate += OnMaxHealthUpdated
+        }
+
+        private void OnDisable()
+        {
+            PlayerState.OnPlayerHitPointsUpdate -= OnHealthUpdated;
+            //TODO: PlayerState.OnPlayerPowerUpsUpdate -= OnMaxHealthUpdated
+        }
+
+        private void OnHealthUpdated(int newValue)
+        {
+            currentHealth = newValue;
+            
+            // Clamp current health in case of overhealing
+            if (currentHealth > maxHealth) currentHealth = maxHealth;
+            // Clamp current health in case of damaging Entity below 0
+            if (currentHealth < 0) currentHealth = 0;
+            
             
             for (int i = 0; i < hearts.Length; i++)
             {
-                if (i < currentHealth)
-                {
-                    hearts[i].sprite = fullHeart;
-                }
-                else
-                {
-                    hearts[i].sprite = emptyHeart;
-                }
-                if (i < maxHealth)
-                {
-                    hearts[i].enabled = true;
-                }
-                else
-                {
-                    hearts[i].enabled = false;
-                }                
+                hearts[i].sprite = (i < currentHealth) ? fullHeart : emptyHeart;
             }
         }
 
-        private void Awake()
+        private void Update()
         {
-            
+            // Display/Change Max Health
+            // TODO: Has to subscribe to OnPlayerPowerUpsUpdate in PlayerState.cs once implemented
+            for (int i = 0; i < hearts.Length; i++) hearts[i].enabled = i < maxHealth;
         }
-    
-        public void UpdateBar(int newValue)
-        {
-           
-
-        }
-
-        private void ChangeSprite()
-        {
-        }
-    
-    
     }
 }
-                     
