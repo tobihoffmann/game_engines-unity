98c380aec842050699408b4bb8820963813e16b7
Tobi Hoffmann <tobi.hoffmann@protonmail.com>
2020-11-17 13:38:22 +0100

    BulletCollider.cs was in the wrong namespace

--- Assets/Scripts/Entity/BulletCollider.cs
+++ Assets/Scripts/Entity/BulletCollider.cs
@@ -1,18 +1,21 @@
 ﻿
 using UnityEngine;
 
-public class BulletCollider : MonoBehaviour
+namespace Entity
 {
-    [SerializeField][Tooltip("Gameobject for HitEffect Animation")]
-    private GameObject hitEffect;
-
-    void OnCollisionEnter2D(Collision2D collision)
+    public class BulletCollider : MonoBehaviour
     {
+        [SerializeField][Tooltip("Gameobject for HitEffect Animation")]
+        private GameObject hitEffect;
 
-        //initiate explosion effect and destroy bullet object
-        GameObject effect = Instantiate(hitEffect, transform.position, Quaternion.identity);
-        Destroy(effect, 0.15f);
-        Destroy(gameObject);
+        void OnCollisionEnter2D(Collision2D collision)
+        {
 
+            //initiate explosion effect and destroy bullet object
+            GameObject effect = Instantiate(hitEffect, transform.position, Quaternion.identity);
+            Destroy(effect, 0.15f);
+            Destroy(gameObject);
+
+        }
     }
 }
