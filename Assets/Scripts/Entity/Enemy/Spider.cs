d054e96a9f62a65f4d2f70f2cbf7b1a04b326b18
jakobhes <jheschl95@gmail.com>
2020-11-21 13:27:01 +0100

    added explosion animation, added spider prefab

--- Assets/Scripts/Entity/Enemy/Spider.cs
+++ Assets/Scripts/Entity/Enemy/Spider.cs
@@ -1,4 +1,5 @@
-﻿using Entity.Player;
+﻿using System.Collections;
+using Entity.Player;
 using Managers;
 using Pathfinding;
 using UnityEngine;
@@ -10,67 +11,96 @@ namespace Entity.Enemy
         [SerializeField][Tooltip("Distance in which enemy explodes.")]
         private float explodeDistance = 3f;
 
-        [SerializeField] [Tooltip("Distance in which enemy chases the target.")]
+        [SerializeField][Tooltip("Distance in which enemy chases the target.")]
         private float chaseDistance = 10f;
                 
-        [SerializeField] [Tooltip("Amount of damage the explosion deals.")]
+        [SerializeField][Tooltip("Amount of damage the explosion deals.")]
         private int damage = 1;
 
-        //[SerializeField] [Tooltip("Target to chase.")]
-        private GameObject _target;
+        [SerializeField] [Tooltip("Time in seconds until spider explodes.")]
+        private float exTime = .5f;
+        
+        private Animator _animator;
+        
+        //private bool _isExploding = false;
 
-        private GameObject _player;
+        [SerializeField][Tooltip("Target to chase.")]
+        private GameObject target;
+        
         
         private void Awake()
         {
-            _target = PlayerManager.Instance.GetPlayer();
+            target = PlayerManager.Instance.GetPlayer();
             Origin = gameObject;
-            
             AIDestSetter = GetComponent<AIDestinationSetter>();
-            Debug.Log(AIDestSetter.target);
-            PlayerState = _target.GetComponent<PlayerState>();
+            PlayerState = target.GetComponent<PlayerState>();
+            _animator = GetComponent<Animator>();
         }
 
 
         private void Update()
         {
-            
-            
-            Distance = Vector2.Distance(Origin.transform.position, _target.transform.position);
+            Distance = Vector2.Distance(Origin.transform.position, target.transform.position);
             if (Distance < explodeDistance)
                 SwitchState(State.Attack);
-            else if (Distance < chaseDistance)
+            else if (Distance < chaseDistance && _animator.GetBool("isExploding") == false)
                 SwitchState(State.Chase);
             else 
                 SwitchState(State.Idle);
-                
         }
 
         protected override void Idle()
         {
+            if (AIDestSetter.target != null && AIDestSetter.target != Origin.transform)
+                AIDestSetter.target = Origin.transform;
             //TODO: implement patrolling or sth
         }
 
+        /// <summary>
+        /// Attack method of spider. Spider stops chasing and explodes.
+        /// </summary>
         protected override void Attack()
         {
             AIDestSetter.target = Origin.transform;
-            
-            Vector2 originPosition = Origin.transform.position;
-            Vector2 targetPosition = new Vector2(_target.transform.position.x, _target.transform.position.y - .6f);
-            RaycastHit2D hit = Physics2D.Raycast(originPosition, (targetPosition - originPosition).normalized, explodeDistance);
-            //Debug.Log(Distance);
-            Debug.DrawRay(originPosition, (targetPosition - originPosition), Color.red);
 
-            //if target not behind cover, deal damage.
-            if (hit.collider.gameObject == _target)
+            //Box Collider Offset
+            Vector2 bcoffset = target.GetComponent<BoxCollider2D>().offset;
+            
+            //Position of origin (spider)
+            Vector2 originPosition = Origin.transform.position;
+            
+            Vector2 targetPosition = target.transform.position;
+            
+            //Position of collider of the target (player)
+            Vector2 finalTargetPos = targetPosition + bcoffset;
+            
+            RaycastHit2D hit = Physics2D.Raycast(originPosition, (finalTargetPos - originPosition).normalized, explodeDistance+1);
+            Debug.DrawRay(originPosition, (finalTargetPos - originPosition), Color.red);
+            StartCoroutine(WaitAndExplode(exTime, hit));
+        }
+
+        /// <summary>
+        /// Spider waits and then explode. Player gets damage, if not behind cover.
+        /// </summary>
+        /// <param name="explodeTime">Time in seconds until spider explodes.</param>
+        /// <param name="hit">The hit Raycast from the spider to the player.</param>
+        /// <returns></returns>
+        private IEnumerator WaitAndExplode(float explodeTime, RaycastHit2D hit)
+        {
+            
+            yield return new WaitForSeconds(explodeTime);
+            _animator.SetBool("isExploding", true);
+            yield return new WaitForSeconds(0.5f);
+            if (hit.collider != null && hit.collider.gameObject == target)
             {
                 PlayerState.Hit(damage);
             }
+            Destroy(gameObject);
         }
 
         protected override void Chase()
         {
-            AIDestSetter.target = _target.transform;
+            AIDestSetter.target = target.transform;
         }
     }
 }
