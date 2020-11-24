590990f7d5a721a08a8b97d21380618faaed4896
jakobhes <jheschl95@gmail.com>
2020-11-24 13:18:21 +0100

    added level complete scene, gameManager and trigger prefab, new font

---
+++ Assets/Scripts/Managers/GameManager.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace Managers
+{
+    public class GameManager : MonoBehaviour
+    {
+        private bool _gameHasEnded = false;
+
+        public void CompleteLevel()
+        {
+            Debug.Log("Level won!");
+            SceneManager.LoadScene("Scenes/LevelComplete");
+        }
+    
+        public void EndGame()
+        {
+            if (_gameHasEnded == false)
+            {
+                _gameHasEnded = true;
+                Debug.Log("Game Over!");
+            }
+        }
+
+        void Restart()
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        }
+
+    }
+}
