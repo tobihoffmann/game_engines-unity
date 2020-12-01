f8994bfaf1566e7a70cbc3ce0b6fd57c716266ea
tobihoffmann <38102896+tobihoffmann@users.noreply.github.com>
2020-12-01 19:27:47 +0100

    Audio manager (#53)
    * hhihihi
    
    * requested changes
    
    * init
    
    * added sounds
    
    * spider sound
    
    * Player sound effects, Soundtrack
    
    * spider rigidbody changes

---
+++ Assets/Scripts/Audio/SpiderAudio.cs
@@ -0,0 +1,18 @@
+﻿using Managers;
+using UnityEngine;
+
+namespace Audio
+{
+    public class SpiderAudio : MonoBehaviour
+    {
+        private void Beep()
+        {
+            AudioManager.Instance.Play("SpiderBeep");
+        }
+        
+        private void Explode()
+        {
+            AudioManager.Instance.Play("SpiderExplosion");
+        }
+    }
+}
