115232a8bef6e11f94b12156eb115e4d8a02bc73
Ben <b.naujoks.roldan@googlemail.com>
2020-12-01 21:01:49 +0100

    Button trigger test

---
+++ Assets/Scripts/Item Management/ItemWorldSpawner.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Assets.Scripts.Item_Management;
+using UnityEngine;
+
+public class ItemWorldSpawner : MonoBehaviour
+{
+    public Item item;
+
+    private void Start()
+    {
+        ItemWorld.SpawnItemWorld(transform.position, item);
+        Destroy(gameObject);
+    }
+}
