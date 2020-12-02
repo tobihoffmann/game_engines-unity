cd069a62cecfe097d83a6fbfd373e50e411b7686
jakobhes <jheschl95@gmail.com>
2020-12-02 16:19:56 +0100

    added game over functionality and scene.

---
+++ Assets/Scripts/SceneManagement/LevelComplete.cs
@@ -0,0 +1,18 @@
+﻿using Managers;
+using UnityEngine;
+
+namespace SceneManagement
+{
+    public class LevelComplete : MonoBehaviour
+    {
+        /// <summary>
+        /// If the player walks on the trigger, the level completes.
+        /// </summary>
+        /// <param name="other"></param>
+        private void OnTriggerEnter2D(Collider2D other)
+        {
+            if (other == PlayerManager.Instance.GetPlayer().GetComponent<Collider2D>())
+                GameManager.Instance.ChangeLevel();
+        }
+    }
+}
