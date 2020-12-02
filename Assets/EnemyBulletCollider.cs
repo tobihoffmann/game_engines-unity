7114890935fcdd9db0a50520d981e225a17fa2b9
jakobhes <jheschl95@gmail.com>
2020-12-02 18:35:45 +0100

    added init ranged demon enemy. player doesn't get damage bug, not fixed yet

---
+++ Assets/EnemyBulletCollider.cs
@@ -0,0 +1,25 @@
+﻿using AbstractClasses;
+using Entity.Player;
+using Managers;
+using UnityEngine;
+
+public class EnemyBulletCollider : MonoBehaviour
+{
+    [SerializeField][Tooltip("Gameobject for HitEffect Animation")]
+    private GameObject hitEffect;
+
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.collider.GetComponent<Damageable>())
+        {
+            var damage = 1;
+            PlayerManager.Instance.GetPlayer().GetComponent<PlayerState>().Hit(damage);
+        }
+            
+        //initiate explosion effect and destroy bullet object
+        GameObject effect = Instantiate(hitEffect, transform.position, Quaternion.identity);
+        Destroy(effect, 0.15f);
+        Destroy(gameObject);
+    }
+}
