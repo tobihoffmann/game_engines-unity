18f4d86c4bc02e023140a70cdac114f5fb511e50
Tobi Hoffmann <tobi.hoffmann@protonmail.com>
2020-12-06 20:53:54 +0100

    Refactoring and More :)

---
+++ Assets/TestItemSpawner.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Item_Management;
+using UnityEngine;
+
+public class TestItemSpawner : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        ItemWorld.SpawnItemWorld(new Vector3(2, 2), new Item {itemType = Item.ItemType.JuggernautBuff});
+        ItemWorld.SpawnItemWorld(new Vector3(-2, -1), new Item {itemType = Item.ItemType.SpeedBuff});
+        ItemWorld.SpawnItemWorld(new Vector3(4, 4), new Item {itemType = Item.ItemType.ShootDamageBuff});
+    }
+}
