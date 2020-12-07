c7402e8363d471e0cf4bf15233502e0bba1b3aab
Jakob <jheschl95@gmail.com>
2020-12-07 22:23:39 +0100

    healthkit

---
+++ Assets/HealthKit.cs
@@ -0,0 +1,18 @@
+﻿using Entity.Player;
+using Managers;
+using UnityEngine;
+
+public class HealthKit : MonoBehaviour
+{
+    [SerializeField]
+    private int healAmount;
+
+    PlayerState ps;
+    
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        ps = PlayerManager.Instance.GetPlayerState();
+        ps.Heal(healAmount);
+    }
+}
